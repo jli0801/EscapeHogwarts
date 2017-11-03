0450994d7d56c8abafc5d8a26008f029b020ed0a
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-11-03 15:10:38 -0400

    Committtt

---
+++ EscapeHogwarts/src/kevinStephRoom/KevinRoom.java
@@ -0,0 +1,12 @@
+package kevinStephRoom;
+
+import caveExplorer.NPCRoom;
+
+public class KevinRoom extends NPCRoom {
+
+	public KevinRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ EscapeHogwarts/src/kevinStephRoom/StephanieRoom.java
@@ -0,0 +1,12 @@
+package kevinStephRoom;
+
+import caveExplorer.NPCRoom;
+
+public class StephanieRoom extends NPCRoom {
+
+	public StephanieRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
