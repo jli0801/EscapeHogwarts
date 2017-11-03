53ce458368346fbb01a29f731c6f2c325ba1c204
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-03 15:08:32 -0400

    branches

---
+++ EscapeHogwarts/src/jiVickieMinigame/VickieRoom.java
@@ -0,0 +1,12 @@
+package jiVickieMinigame;
+
+import caveExplorer.CaveRoom;
+
+public class VickieRoom extends CaveRoom {
+
+	public VickieRoom(String description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
