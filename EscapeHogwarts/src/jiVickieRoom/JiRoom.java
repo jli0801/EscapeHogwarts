493bbfd6ea41e8c6138567dfb8a47247a916c942
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-06 14:37:04 -0500

    c

--- EscapeHogwarts/src/jiVickieRoom/JiRoom.java
+++ EscapeHogwarts/src/jiVickieRoom/JiRoom.java
@@ -1,12 +1,38 @@
 package jiVickieRoom;
 
+import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
+import caveExplorer.NPC;
+import caveExplorer.NPCRoom;
 
 public class JiRoom extends CaveRoom {
+	
+	private NPC npc;
 
 	public JiRoom(String description) {
 		super(description);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public String validMoves() {
+		return "wdsaeo";
+	}
+	
+	public void printValidMoves() {
+		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move "
+				+ "or you can press 'e' to interact and 'o' to observe.");
+		
+	}
+	
+	public void performAction(int direction) {
+		if(direction == 4) {
+			if(npc != null && npc.isActive()) {
+				npc.interact();
+			}else {
+				CaveExplorer.print("There is nothing to interact with.");
+			}
+		}else {
+			CaveExplorer.print("That key does nothing.");
+		}
+	}
 }
