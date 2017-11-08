6524933ef25b32535e841389e4e1730cdbf7cab9
jye6013 <jye6013@bths.edu>
2017-11-07 22:10:04 -0500

    update

--- EscapeHogwarts/src/jiVickieRoom/JiRoom.java
+++ EscapeHogwarts/src/jiVickieRoom/JiRoom.java
@@ -1,6 +1,8 @@
 package jiVickieRoom;
 
+import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
+import caveExplorer.Inventory;
 
 public class JiRoom extends CaveRoom {
 
@@ -9,9 +11,32 @@ public class JiRoom extends CaveRoom {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void userEntered() {
-		// TODO Auto-generated method stub
-		
+	public String validMoves() {
+		return "wdsaeo";
+	}
+	
+	public void printValidMoves() {
+		System.out.println("You can only enter 'w', 'a', 's', or 'd' to move "
+
+				+ "or you can press 'e' to interact and 'o' to observe.");
 	}
 
+	public void performAction(int direction) {
+		if(direction == 5) {
+			//what happens when o is pressed
+			CaveExplorer.print("The ancient walls loom over you.");
+
+		}else {
+			//when o is not pressed
+			super.performAction(direction);
+		}
+	}
+
+	public static void userEntered() {
+		CaveExplorer.print("On a far ledge of wall, you find a dusty silver key but you did not manage to reach it.");
+		CaveExplorer.print("But when you jumped back down, you hurt yourself in the landing and lost 30 hp.");
+		int userHp = Inventory.getHp() - 30;
+		Inventory.setHp(userHp);
+		CaveExplorer.print("Your HP is now: " + userHp + ".");
+	}
 }
