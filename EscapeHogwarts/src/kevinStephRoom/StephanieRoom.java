b13543c9876bbaea6cc8a20d91f514b347fff95b
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-09 15:18:45 -0500

    stephroom

--- EscapeHogwarts/src/kevinStephRoom/StephanieRoom.java
+++ EscapeHogwarts/src/kevinStephRoom/StephanieRoom.java
@@ -1,17 +1,64 @@
 package kevinStephRoom;
 
-import java.util.Scanner;
-
 import caveExplorer.CaveExplorer;
 import caveExplorer.CaveRoom;
-import caveExplorer.Door;
-import caveExplorer.NPC;
+import caveExplorer.Inventory;
 import caveExplorer.NPCRoom;
+import caveExplorer.NPC;
 
 public class StephanieRoom extends NPCRoom {
-
+	
+	NPC npc;
+	
 	public StephanieRoom(String description) {
 		super(description);
+		
 	}
-
-}
+	
+	public String validMoves() {
+		return "wdsa";
+	}
+	
+	public void printValidMoves()
+	{
+		System.out.println("You can only enter 'w', 'a', 's', or 'd'");
+	}
+	
+	public void interpretInput(String input) {
+		while(!isValid(input)) {
+			printValidMoves();
+			
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction =  validMoves().indexOf(input); 
+		if(direction < 4) {
+			goToRoom(direction);
+		}else {
+			performAction(direction);
+		}
+		
+	}
+	
+	private boolean isValid(String input) {
+		return validMoves().indexOf(input) != -1 && input.length() == 1;
+	}
+	
+	public void performAction (int direction)
+	{
+		if(direction == 4) {
+			if(npc != null && npc.isActive()) {
+				npc.interact();
+			}else {
+				CaveExplorer.print("There is nothing to interact with.");
+			}
+		}else {
+			CaveExplorer.print("That key does nothing.");
+		}
+	}
+	
+	public static void userEntered() {
+		CaveExplorer.print("You see a mirror in front of you");
+		
+	}
+}	
+	
