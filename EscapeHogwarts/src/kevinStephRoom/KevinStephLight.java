1aa05f22053315043f8151280db0d3d9cbe94409
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-16 15:32:01 -0500

    st

--- EscapeHogwarts/src/kevinStephRoom/KevinStephLight.java
+++ EscapeHogwarts/src/kevinStephRoom/KevinStephLight.java
@@ -3,9 +3,12 @@ package kevinStephRoom;
 public class KevinStephLight {
 
 	private boolean lightOn;
+	private int col;
+	private int row;
 
 	public KevinStephLight(int row, int col) {
-		
+		this.row = row;
+		this.col = col;
 	}
 
 	public void lightOn(boolean lightOn) { //main method
