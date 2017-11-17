c1b2bae7713532d5ada70a1ec742deaa3c265b60
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-11-17 15:26:03 -0500

    c

--- EscapeHogwarts/src/jiVickieRoom/JiBackEnd.java
+++ EscapeHogwarts/src/jiVickieRoom/JiBackEnd.java
@@ -14,8 +14,10 @@ public class JiBackEnd implements VickieSupport {
 	private String currentNumber;
 	private int newNumber;
 	private String usedNumbers;
-	private String[] outerNumbers = {};
-	private String[] innerNumbers = {};
+	private String outerNumbers = "2648";
+	private String innerNumbers = "7931";
+	private int initiatedNum;
+	private String StrInitiatedNum;
 
 	public JiBackEnd(JiSupport frontend) {
 		this.frontend = frontend;
@@ -29,16 +31,6 @@ public class JiBackEnd implements VickieSupport {
 				magicSquares[row][col] = new JiVickieBoard(row, col);
 			}
 		}
-		
-		//method for inputting number
-	}
-
-	public void setNumbers(String numbers) { //rename!!!!!
-		this.numbers = numbers;
-	}
-	
-	public String getNumbers() {
-		return numbers;
 	}
 
 	public boolean checkValid(String character) {
@@ -54,11 +46,11 @@ public class JiBackEnd implements VickieSupport {
 		if(checkValid(character) == true) {
 			newNumber = Integer.parseInt(currentNumber);
 			if(usedNumbers().indexOf(newNumber) > -1) {
-				//replace original number with space
-				//add number in new space
+				//replace original number with space (FRONT END)
+				//add number in new space (FRONT END)
 			}else {
 				usedNumbers += Integer.toString(newNumber);
-				//add number in new space
+				//add number in new space (FRONT END)
 			}
 		}
 	}
@@ -76,8 +68,28 @@ public class JiBackEnd implements VickieSupport {
 		
 	}
 
-	public void generateNumbers(int max) {;
+	public int generateNumber(int max) {;
 		random = (int)(Math.random() * max);
+		return random;
+	}
+	
+	public boolean isOuterNumber() {
+		initiatedNum = generateNumber(9);
+		StrInitiatedNum = Integer.toString(initiatedNum);
+		if(outerNumbers.indexOf(StrInitiatedNum) > -1) {
+			return true;
+		}else {
+			if(innerNumbers.indexOf(StrInitiatedNum) > -1) {
+				return false;
+			}else {
+				getMid();
+			}
+		}
+		return false;
+	}
+	
+	public int getMid() {
+		return 5;
 	}
 
 	public void cheatCode() {
--- EscapeHogwarts/src/jiVickieRoom/VickieSupport.java
+++ EscapeHogwarts/src/jiVickieRoom/VickieSupport.java
@@ -24,6 +24,15 @@ public interface VickieSupport {
 		//	System.out.println("You can only type a number.");
 		//}
 	
+	int generateNumber(int max);
+	//generates number based on maximum integer
+	
+	boolean isOuterNumber();
+	//returns true if outer else false
+	
+	int getMid();
+	//outlier method when number not outer or inner, mid can only equal 5
+	
 	void cheatCode();
 	//cheatCode
 
