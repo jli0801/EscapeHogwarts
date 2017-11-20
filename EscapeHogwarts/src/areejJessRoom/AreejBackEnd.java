ab1f609bf6e5a26350f07f59ca97fc7a5dc04d8d
jessi <jessi@DESKTOP-8072EKE.home>
2017-11-19 20:26:27 -0500

    display

--- EscapeHogwarts/src/areejJessRoom/AreejBackEnd.java
+++ EscapeHogwarts/src/areejJessRoom/AreejBackEnd.java
@@ -2,12 +2,13 @@ package areejJessRoom;
 
 import java.util.Arrays;
 import areejJessRoom.JessicaFrontEnd;
+import caveExplorer.CaveExplorer;
 
 public class AreejBackEnd implements JessicaSupport{
 
 	private AreejSupport frontend;
 	private static int[][] board;
-	
+
 	
 	//0 EQUALS NULL
 	//1 EQUALS USER
@@ -18,6 +19,7 @@ public class AreejBackEnd implements JessicaSupport{
 		board = new int[6][7];
 	//	board = AreejJessBoard.getBoard();
 		populateBoard(board);
+	
 		
 	}
 	private int row;
@@ -57,10 +59,10 @@ public class AreejBackEnd implements JessicaSupport{
 				String place = Integer.toString(board[i][j]);
 				
 				if(place.equals("U")) {
-					JessicaFrontEnd.isUserWon(true);
+					JessicaFrontEnd.setUserWon(true);
 				}
 				else if(place.equals("C")) {
-					JessicaFrontEnd.isComputerWon(true);
+					JessicaFrontEnd.setComputerWon(true);
 				}
 			}
 		}
@@ -84,24 +86,75 @@ public class AreejBackEnd implements JessicaSupport{
 		return false;
 	}
 
-	public static void moveUser(int userInt) {
+	public static void moveUser() {
 		
-	//	int[][] board = AreejJessBoard.getBoard();
-	//	populateBoard(board);
-		String[][] boardFront = JessicaFrontEnd.getBoard();
+
+	//	String[][] boardFront = JessicaFrontEnd.getBoard();
+		
+		String userInput = CaveExplorer.in.nextLine();
+
+		
 		board = new int[6][7];
 		populateBoard(board);
-		//maybe no boolean arr but just string array
 		
-				if(board[5][userInt] == 0)
+	//	System.out.println("Where would you like to place your Galleon? Pick a number from 0 to 6.");
+		
+		if(userInput.equals("voldemort"))
+		{
+			System.out.println("No! Not Voldemort! You win, leave my room!");
+			JessicaFrontEnd.setUserWon(true);
+		}
+		else
+		{
+		
+	//		int userInt = Integer.parseInt(userInput);
+			if(checkValid(userInput))
+			{
+				int userInt = Integer.parseInt(userInput);
+				System.out.println("in");
+				for(int i = board.length - 1; i > 0; i--)
 				{
-					board[5][userInt] = 1;
-					JessicaFrontEnd.placeCoord(5,userInt, "user");
-				//	System.out.print(Arrays.toString(board));
+					System.out.println("for");
+					if(board[i][userInt] == 0)
+					{
+						System.out.println("empty");
+						board[i][userInt] = 1;
+						JessicaFrontEnd.placeCoord(i,userInt, "user");
+					//	System.out.print(Arrays.toString(board));
+					}
+					while (board[6][userInt] != 0 && !JessicaFrontEnd.isUserWon())
+					{
+						System.out.println("The column is full. Pick another column!");
+						moveUser();
+					}
 				}
-			
-			
+				
+			}
+			else
+			{
+				System.out.println("Pick a number between 0 and 6!");
+				moveUser();
+			}
+		}
 	
+	}
+	
+	private static boolean checkValid(String userInput) {
+		
+		try{
+			int userInt = Integer.parseInt(userInput);
+		}
+		catch(NumberFormatException nfe){
+			return false;
+		}
+		int userInt = Integer.parseInt(userInput);
+		if( userInt <= 6 && userInt >= 0)
+		{
+			
+			return true;
+		}
+		
+		return false;
 	}
 
 	private static void populateBoard(int[][] board2) {
